28cf7e2b50d50baac286662903984957ad14bf17
George Higbie <gehigbie@gmail.com>
2016-10-21 13:05:55 -0400

    created TurkeyBig object added animationa and sound effects

--- app/src/main/java/com/geogehigbie/turkeyshoot/MainActivity.java
+++ app/src/main/java/com/geogehigbie/turkeyshoot/MainActivity.java
@@ -1,7 +1,9 @@
 package com.geogehigbie.turkeyshoot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initialAnimation();
+    }
+
+    public void initialAnimation(){
+        ImageView turkeybody = (ImageView) findViewById(R.id.turkey_body);
+        TurkeyBig turkeyBig1 = new TurkeyBig(turkeybody, 2000, 2000, 2000, -2000, -1700, 1700, 4000, 0, 0, false);
+        turkeyBig1.translateAninimation(turkeybody);
+
+        playInitialGobble();
 
     }
+
+    public void playInitialGobble(){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.turkey_gobble);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(false);
+
+    }
+
+    public void playSoundEffects(){
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.click_on_sound);
+        mediaPlayer.start();
+
+    }
+
+
+
 }
